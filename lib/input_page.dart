e81328c8a3440db17cf0c9f6358935315732e5bd
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-14 23:12:33 +0900

    fix: dart-sdkのバージョンによるエラーを修正
    refs #13 dartとflutterのバージョンを変更し、バージョンによるエラーを回避

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 // import
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 // 変数
@@ -59,7 +60,7 @@ class ReusableCard extends StatelessWidget {
   final Color color;
 
   // コンストラクタ
-  ReusableCard({@required this.color});
+  ReusableCard({required this.color});
 
   @override
   Widget build(BuildContext context) {
