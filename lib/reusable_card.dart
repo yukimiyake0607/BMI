e093877c7123cb2c2e927caa516a36618873eaad
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-16 20:32:50 +0900

    update: ReusableCardクラスにonPressプロパティを追加
    refs #27 コード簡略化、可読性向上のため

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 
-
 // class(ReusableCard)
 class ReusableCard extends StatelessWidget {
   // プロパティ
   final Color color;
   final Widget? cardChild;
+  final VoidCallback? onPress;
 
   // コンストラクタ
-  ReusableCard({required this.color, this.cardChild});
+  ReusableCard({required this.color, this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
