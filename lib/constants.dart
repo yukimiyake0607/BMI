98bdcfea7283185da23aa28617055fa8b8d4418a
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-17 14:50:00 +0900

    add: グローバル定数を設定
    refs #33 アプリ全体で使用するため

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,12 +5,9 @@ const Color kInactiveReusableCardColor = Color(0xFF23263A);
 const kBottomContainerColor = Color(0xFFEB1555);
 const kBottomContainerHeight = 80.0;
 
-final kGenderTestStyle = TextStyle(
+final kLabelTextstyle = TextStyle(
   fontSize: 18,
   color: Color(0xFF8D8E98),
 );
 
-final kNumberTextstyle = TextStyle(
-  fontSize: 50,
-  fontWeight: FontWeight.w900
-);
+final kNumberTextstyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
