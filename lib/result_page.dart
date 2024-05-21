f451ab1a1ac6171a0787e98caac0f63b9e7e1c15
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-21 14:57:15 +0900

    add: 仮のページを作成
    refs #49 今後中身記述

---
+++ lib/result_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
