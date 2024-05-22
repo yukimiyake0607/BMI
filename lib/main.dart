98e213ed70fa3a6b698f258364474694e2c2c201
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-22 17:20:37 +0900

    update: ルート管理をやめて、homeプロパティを追加
    refs #60 CALCULATEボタンを押下した際に引数を渡すため

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      home: InputPage(),
       theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xFF1D2135),
         ),
         scaffoldBackgroundColor: Color(0xFF1D2135),
       ),
-      routes: {
-        '/':(context) => InputPage(),
-        '/calculator':(context) => ResultPage(),
-      },
     );
   }
 }
