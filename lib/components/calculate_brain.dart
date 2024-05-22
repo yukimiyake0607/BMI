250b994dda6f7f4f1ad8a51078cdc3ac4945f66e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-22 17:35:39 +0900

    update: null非許容に変更

--- lib/components/calculate_brain.dart
+++ lib/components/calculate_brain.dart
@@ -3,20 +3,20 @@ import 'dart:math';
 class CalculateBrain {
   CalculateBrain({required this.height, required this.weight});
 
-  int? height;
-  int? weight;
+  int height;
+  int weight;
 
-  double? _bmi;
+  late double _bmi;
 
-  String calcurate() {
-    _bmi = weight! / pow(height! / 100, 2);
-    return _bmi!.toStringAsFixed(1);
+  String calculate() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi! >= 25.0) {
+    if (_bmi >= 25.0) {
       return 'Overweight!';
-    } else if (_bmi! > 18.5) {
+    } else if (_bmi > 18.5) {
       return 'Nomral';
     } else {
       return 'Underweight!';
@@ -24,9 +24,9 @@ class CalculateBrain {
   }
 
   String getAdvice() {
-    if (_bmi! >= 25.0) {
+    if (_bmi >= 25.0) {
       return 'あなたは普通より太っています。もっと運動しましょう。';
-    } else if (_bmi! > 18.5) {
+    } else if (_bmi > 18.5) {
       return 'いいですね！このまま続けましょう！';
     } else {
       return 'あなたは普通より痩せています。もっと食べましょう。';
