5337ee72c6da45f326041db3f4338b5d45896127
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-22 15:02:20 +0900

    add: BMI結果ページの大枠を作成
    refs #52

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -9,8 +11,44 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        child: Text('Hello'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Result',
+                style: kResultTitleTextstyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveReusableCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '20.3',
+                    style: kBMITextstyle,
+                  ),
+                  Text(
+                    'Your BMI result is quite low. You should eat more!',
+                    style: kBMIBodyTextstyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
